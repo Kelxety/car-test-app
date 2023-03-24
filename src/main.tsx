d1b2ff950ea4406c098bd934073a3de1dc66b910
Kelxety <llanzanarobinjohn@gmail.com>
2023-03-25 00:06:10 +0800

    remove route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes/routes";
 import Nav from "./components/Nav";
 import "./index.css";
