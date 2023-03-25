e7ea3223edc55a2d00a0b0b017476a9961543949
Kelxety <llanzanarobinjohn@gmail.com>
2023-03-25 16:26:38 +0800

    update ui of car details

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -23,7 +23,7 @@ function Car() {
             orientation: "landscape",
           })
           .then((result) => {
-            setPhotos(result?.response?.results.splice(0, 5));
+            setPhotos(result?.response?.results.splice(0, 7));
           });
       },
     }
@@ -35,49 +35,76 @@ function Car() {
   if (isError)
     return <div className="w-full flex items-center justify-center">Error</div>;
   return (
-    <div className="max-w-7xl flex items-center justify-center mx-auto">
-      <div className="w-full flex items-center justify-center">
-        <div className="container mx-auto">
-          <div className="grid-cols-3 p-2 sm:p-20 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+    <section className="text-gray-700 body-font overflow-hidden bg-white">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <img
+            alt="ecommerce"
+            className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
+            src={photos?.[0]?.urls?.regular}
+          />
+          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+            <div className="flex justify-between">
+              <div>
+                <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                  BRAND NAME
+                </h2>
+                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                  {data?.Car?.car}
+                </h1>
+              </div>
+              <div>
+                <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                  BRAND MODEL
+                </h2>
+                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                  {data?.Car?.car_model}
+                </h1>
+              </div>
+            </div>
+            <span className="flex items-center mt-4">
+              <span className="text-gray-600">
+                <span className="font-bold">VIN</span> {data?.Car.car_vin}
+              </span>
+            </span>
+            <div>
+              <p className="leading-relaxed mt-4 text-sm">
+                Simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+              </p>
+            </div>
+            <div className="p-4 border rounded mt-4">
+              <h5 className="font-semibold">History of Accident</h5>
+              <p className="leading-relaxed mt-2 text-sm">
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged
+              </p>
+            </div>
+            <div className="sm:flex mt-2 items-center pb-5 border-b-2 border-gray-200 mb-5 hidden"></div>
+            <div className="flex mt-4 sm:mt-0">
+              <span className="title-font font-medium sm:text-2xl text-gray-900">
+                Price: {data?.Car.price}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <div className="grid-cols-6 p-2 sm:p-10  lg:grid lg:gap-3 overflow-auto">
             {photos?.map((photo: any, i: number) => {
               {
-                if (i === 0) {
-                  return (
-                    <Fragment key={i}>
-                      <div className="w-full col-span-2 row-span-2 rounded">
-                        <PhotoComp photo={photo} />
-                      </div>
-                      <div className="w-full rounded">
-                        <div className="flex items-center justify-between">
-                          <div>
-                            <h1 className="text-2xl font-bold">
-                              {data?.Car?.car_model}
-                            </h1>
-                            <h2 className="font-semibold">{data?.Car?.car}</h2>
-                          </div>
-                          <div>
-                            <h1 className="text-2xl font-bold text-blue-500">
-                              {data?.Car?.price}
-                            </h1>
-                          </div>
-                        </div>
-                      </div>
-                      <div>This is Text</div>
-                    </Fragment>
-                  );
-                } else {
+                if (i > 0)
                   return (
                     <div key={i} className="w-full rounded">
                       <PhotoComp photo={photo} />
                     </div>
                   );
-                }
               }
             })}
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
