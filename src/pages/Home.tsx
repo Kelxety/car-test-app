22126fb7410ed86c6b7d7a1a736077e540fbde37
Kelxety <llanzanarobinjohn@gmail.com>
2023-03-25 13:08:59 +0800

    added fetching images in car list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,33 @@ import { useQuery } from "react-query";
 import CarList from "../components/Home/CarList";
 import Pagination from "../components/Home/Pagination";
 import fetchCars from "../utils/api/fetchCars";
+import PhotoComp from "../components/Car/PhotoComp";
+import { createApi } from "unsplash-js";
 
 import { CarT } from "../utils/types/CarTypes";
 
+const unsplash = createApi({
+  accessKey: import.meta.env.VITE_ACCESS_KEY || "",
+});
+
 function Home() {
-  const { data, isLoading, isError } = useQuery("cars", () => fetchCars());
+  const { data, isLoading, isError } = useQuery("cars", () => fetchCars(), {
+    onSuccess: (element) => {
+      element.cars.map((car: CarT, i: number) => {
+        if (i < 20) {
+          unsplash.search
+            .getPhotos({
+              query: car.car + " " + car.car_model,
+              orientation: "landscape",
+            })
+            .then((result) => {
+              car.photo = result?.response?.results[0];
+            });
+        }
+      });
+    },
+  });
+
   const [numberOfItems, setNumberOfItems] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
   const [option] = useState([5, 10, 20]);
@@ -16,22 +38,13 @@ function Home() {
   if (isError)
     return <div className="flex justify-center items-center">Error</div>;
 
-  const calculateTotalPages = (total: number, perPage: number) => {
-    const range = [];
-    const num = Math.ceil(total / perPage);
-    for (let i = 1; i <= num; i++) {
-      range.push(i);
-    }
-    return range;
-  };
-
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-  const totalPages = calculateTotalPages(data?.cars?.length, numberOfItems);
   const sliceData = (data: CarT[], page: number, rowsPerPage: number) => {
     return data.slice((currentPage - 1) * rowsPerPage, page * rowsPerPage);
   };
 
   const cars = sliceData(data?.cars, currentPage, numberOfItems);
+
   return (
     <div className="container p-2 max-w-xl mx-auto sm:px-10 rounded-2xl space-y-4 gap-10">
       <div className="w-full justify-between rounded-xl p-2 flex sm:space-x-2 items-center flex-col space-y-4 sm:space-y-0 sm:flex-row">
