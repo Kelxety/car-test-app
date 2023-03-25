82cb3cdc2774eb9c135ba3630cf2fc1813bf4c72
Kelxety <llanzanarobinjohn@gmail.com>
2023-03-25 14:31:06 +0800

    added on settled setTimeout on fetching photos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import CarList from "../components/Home/CarList";
 import Pagination from "../components/Home/Pagination";
 import fetchCars from "../utils/api/fetchCars";
-import PhotoComp from "../components/Car/PhotoComp";
 import { createApi } from "unsplash-js";
 
 import { CarT } from "../utils/types/CarTypes";
@@ -13,6 +12,7 @@ const unsplash = createApi({
 });
 
 function Home() {
+  const [loadImage, setLoadImage] = useState(false);
   const { data, isLoading, isError } = useQuery("cars", () => fetchCars(), {
     onSuccess: (element) => {
       element.cars.map((car: CarT, i: number) => {
@@ -27,6 +27,11 @@ function Home() {
             });
         }
       });
+    },
+    onSettled: () => {
+      setTimeout(() => {
+        setLoadImage(true);
+      }, 1000);
     },
   });
 
@@ -48,36 +53,40 @@ function Home() {
 
   return (
     <div className="container p-2 max-w-xl mx-auto sm:px-10 rounded-2xl space-y-4 gap-10">
-      <div className="w-full justify-between rounded-xl p-2 flex sm:space-x-2 items-center flex-col space-y-4 sm:space-y-0 sm:flex-row">
-        <Pagination
-          itemsPerPage={numberOfItems}
-          totalItems={data?.cars?.length}
-          paginate={paginate}
-          currentPage={currentPage}
-        />
-        <div className="flex items-center justify-center space-x-2">
-          <h1 className="text-sm">SHOW: </h1>
-          <select
-            value={numberOfItems}
-            onChange={(e) => setNumberOfItems(parseInt(e.target.value))}
-            className=" rounded-lg border p-2"
-          >
-            {option.map((number: number, i: number) => {
-              return (
-                <option key={i} value={number} defaultValue="5">
-                  {number}
-                </option>
-              );
+      {loadImage && (
+        <>
+          <div className="w-full justify-between rounded-xl p-2 flex sm:space-x-2 items-center flex-col space-y-4 sm:space-y-0 sm:flex-row">
+            <Pagination
+              itemsPerPage={numberOfItems}
+              totalItems={data?.cars?.length}
+              paginate={paginate}
+              currentPage={currentPage}
+            />
+            <div className="flex items-center justify-center space-x-2">
+              <h1 className="text-sm">SHOW: </h1>
+              <select
+                value={numberOfItems}
+                onChange={(e) => setNumberOfItems(parseInt(e.target.value))}
+                className=" rounded-lg border p-2"
+              >
+                {option.map((number: number, i: number) => {
+                  return (
+                    <option key={i} value={number} defaultValue="5">
+                      {number}
+                    </option>
+                  );
+                })}
+                ;
+              </select>
+            </div>
+          </div>
+          <div className="bg-slate-100 rounded-lg gap-4 space-y-4 p-4">
+            {cars.map((car: CarT, i: number) => {
+              return <CarList key={i} car={car} />;
             })}
-            ;
-          </select>
-        </div>
-      </div>
-      <div className="bg-slate-100 rounded-lg gap-4 space-y-4 p-4">
-        {cars.map((car: CarT, i: number) => {
-          return <CarList key={i} car={car} />;
-        })}
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
