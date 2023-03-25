9de44dd11b26de825b84e919e8f787238ead1e54
Kelxety <llanzanarobinjohn@gmail.com>
2023-03-25 12:22:21 +0800

    added photo image with credits

---
+++ src/components/Car/PhotoComp.tsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import { CarPhotoT } from "../../utils/types/CarPhotoTypes";
+
+const PhotoComp: React.FC<{ photo: CarPhotoT }> = ({ photo }) => {
+  const { user, urls } = photo;
+
+  return (
+    <Fragment>
+      <img className="img" src={urls.regular} />
+      <a
+        className="credit"
+        target="_blank"
+        href={`https://unsplash.com/@${user.username}`}
+      >
+        <span className="text-sm text-gray-300 hover:text-gray-700">
+          Photo by: {user.name}
+        </span>
+      </a>
+    </Fragment>
+  );
+};
+
+export default PhotoComp;
