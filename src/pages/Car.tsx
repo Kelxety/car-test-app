7fd1de7d6d93e7542e151a6474bd3113635414e6
Kelxety <llanzanarobinjohn@gmail.com>
2023-03-25 12:22:36 +0800

    fetch 10 images by query

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -1,24 +1,86 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import fetchCar from "../utils/api/fetchCar";
-import fetchPhotos from "../utils/api/fetchPhotos";
+import PhotoComp from "../components/Car/PhotoComp";
+import { createApi } from "unsplash-js";
+
+const unsplash = createApi({
+  accessKey: import.meta.env.VITE_ACCESS_KEY || "",
+});
 
 function Car() {
+  const [photos, setPhotos] = React.useState<any>([]);
   const { id } = useParams();
   const { isLoading, isError, data } = useQuery("car", () => fetchCar("" + id));
-  const datas = useQuery("photos", () => fetchPhotos("Innova"));
-  console.log(datas.data);
-  if (isLoading)
+
+  useEffect(() => {
+    if (data) {
+      unsplash.search
+        .getPhotos({
+          query: data.Car.car + " " + data.Car.car_model,
+          orientation: "landscape",
+        })
+        .then((result) => {
+          if (result) {
+            setPhotos(result?.response?.results);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [data]);
+
+  if (isLoading && photos.length === 0)
     return (
       <div className="w-full flex items-center justify-center">Loading...</div>
     );
   if (isError)
     return <div className="w-full flex items-center justify-center">Error</div>;
-  console.log(data);
   return (
     <div className="w-full flex items-center justify-center mx-auto">
-      <div className="w-full flex items-center justify-center"></div>
+      <div className="w-full flex items-center justify-center">
+        <div className="container mx-auto">
+          <div className="grid-cols-3 p-2 sm:p-20 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+            {photos.map((photo: any, i: number) => {
+              {
+                if (i === 0) {
+                  return (
+                    <Fragment key={i}>
+                      <div className="w-full col-span-2 row-span-2 rounded">
+                        <PhotoComp photo={photo} />
+                      </div>
+                      <div className="w-full rounded">
+                        <div className="flex items-center justify-between">
+                          <div>
+                            <h1 className="text-2xl font-bold">
+                              {data?.Car?.car_model}
+                            </h1>
+                            <h2 className="font-semibold">{data?.Car?.car}</h2>
+                          </div>
+                          <div>
+                            <h1 className="text-2xl font-bold text-blue-500">
+                              {data?.Car?.price}
+                            </h1>
+                          </div>
+                        </div>
+                      </div>
+                      <div>This is Text</div>
+                    </Fragment>
+                  );
+                } else {
+                  return (
+                    <div key={i} className="w-full rounded">
+                      <PhotoComp photo={photo} />
+                    </div>
+                  );
+                }
+              }
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
